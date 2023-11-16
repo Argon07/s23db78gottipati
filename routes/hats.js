f406fe6e11524f2aaee4d46d888ed1467eb4cf01
Agraja <agraja.fall2023@gmail.com>
2023-11-15 19:45:15 -0600

    create page added

--- routes/hats.js
+++ routes/hats.js
@@ -4,4 +4,5 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', hat_controlers.hat_view_all_Page );
 router.get('/detail', hat_controlers.hat_view_one_Page);
+router.get('/create', hat_controlers.hat_create_Page);
 module.exports = router
